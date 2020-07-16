54e8a2ee7683f16fc556198c1e1cc2991d42b81d
medericgb <46417515+medericgb@users.noreply.github.com>
2020-07-16 20:33:34 +0000

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-export let users = [
-    {id: 1, name: "Jean", email: "jean@mail.com", age: 17},
-    {id: 2, name: "Rose", email: "rose@mail.com", age: 31},
-    {id: 3, name: "Mario", email: "mario@mail.com", age: 25},
-    {id: 4, name: "Linus", email: "linus@mail.com", age: 88},
-    {id: 5, name: "Maia", email: "maia@mail.com", age: 45}
-]
